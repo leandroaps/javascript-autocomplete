c695501f38b727281e4e408e72a1d12246663712
Leandro A. Siqueira <leandroaps@gmail.com>
2021-12-14 18:18:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import './style.css';
 
 function App() {
+  useEffect(() => {
+    (function () {
+      let inputField = document.getElementById('search');
+      let ulField = document.getElementById('suggestions');
+      inputField.addEventListener('input', changeAutoComplete);
+      ulField.addEventListener('click', selectItem);
+
+      function changeAutoComplete({ target }) {
+        let data = target.value;
+        ulField.innerHTML = ``;
+        if (data.length) {
+          let autoCompleteValues = autoComplete(data);
+          autoCompleteValues.forEach((value) => {
+            addItem(value);
+          });
+        }
+      }
+
+      function autoComplete(inputValue) {
+        let destination = ['Italy', 'Spain', 'Portugal', 'Brazil'];
+        return destination.filter((value) =>
+          value.toLowerCase().includes(inputValue.toLowerCase())
+        );
+      }
+
+      function addItem(value) {
+        ulField.innerHTML =
+          ulField.innerHTML + `<li class="list-group-item">${value}</li>`;
+      }
+
+      function selectItem({ target }) {
+        if (target.tagName === 'LI') {
+          inputField.value = target.textContent;
+          ulField.innerHTML = ``;
+        }
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              id="search"
+            />
+            <button className="btn btn-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      <ul className="list-group" id="suggestions"></ul>
     </div>
   );
 }
